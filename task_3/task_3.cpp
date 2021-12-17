6349d386bc9bf3daf71d8346a27b2655659d0582
blazhkevych <65856963+blazhkevych@users.noreply.github.com>
2021-12-17 13:23:43 +0200

    исправил ошибку в 3 задаче

--- task_3/task_3.cpp
+++ task_3/task_3.cpp
@@ -15,9 +15,8 @@ int main()
 
 	double length{ 0.0 }, width{ 0.0 }, height{ 0.0 }; //длина, ширина, высота 
 	const double paint_consumption{ 0.5 }; //расход краски на 1 квадратный метр
-	double wall_area{ 0.0 }; // площадь стен
+	double wall_area{ 0.0 }, wall_area_2{ 0.0 }; // площадь стен
 	double net_wall_area{ 0.0 }; // площадь стен за вычетом 20% поверхности стен (окна и двери)
-	double perimeter{ 0.0 }; // периметр 
 
 	cout << "Введите длину комнаты: " << endl;
 	cin >> length;
@@ -26,10 +25,10 @@ int main()
 	cout << "Введите высоту комнаты: " << endl;
 	cin >> height;
 
-	perimeter = (length * width) * 2;
-	wall_area = perimeter * height;
+	wall_area = (length * height) * 2;
+	wall_area_2 = (height * width) * 2;
 	//cout << wall_area << endl;
-	net_wall_area = wall_area - (wall_area / 100 * 20);
+	net_wall_area = (wall_area + wall_area_2) - ((wall_area + wall_area_2) / 100 * 20);
 	//cout << net_wall_area << endl;
 
 	cout << '\n' << net_wall_area * paint_consumption << " л. краски уйдет на покраску стен комнаты." << endl;
